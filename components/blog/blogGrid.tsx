214b33cd2a02d358bd4b2611e34f56f2eecce15e
NarekShabandari <narekshabandari01@gmail.com>
2026-08-15 16:35:53 +0400

    consider posts may be undefined

--- components/blog/blogGrid.tsx
+++ components/blog/blogGrid.tsx
@@ -21,7 +21,7 @@ interface Post {
 export function BlogGrid({ posts }: { posts: Post[] }) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-      {posts.map((post) => (
+      {posts?.map((post) => (
         <BlogCard key={post.slug} {...post} />
       ))}
     </div>
